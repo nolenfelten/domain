ddd59da721d390c9a0b2b44986af725e457df19c
Teng Siong Ong <siong1987@gmail.com>
2014-06-03 20:33:45 -0700

    slight optimization.

--- check.rb
+++ check.rb
@@ -4,12 +4,14 @@ TLD = '.me'
 
 File.open("./en.txt") do |file|
   file.each do |line|
-    sleep 1 + rand(10)
     word = line.strip
 
     # different requirements for the word
     next if word.length <= 3
     next if word.length >= 7
+
+    # Don't run it too often
+    sleep 1 + rand(2)
 
     domain = "#{word}#{TLD}"
     puts domain if Whois.available?(domain)
